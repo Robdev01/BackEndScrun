f72862811d71db2ed64a7c9ae3c6a3df8a2820fe
Vivo Monitoramento <vivomonitoramento@outlook.com>
2025-06-03 13:22:11 -0300

    Subindo back pronto

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes import init_routes
 from banco import init_db
 from flask_cors import CORS
+import os
 app = Flask(__name__)
 
 CORS(app)
@@ -11,4 +12,5 @@ init_db()
 init_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
